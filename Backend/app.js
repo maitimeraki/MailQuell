8638fb29cb41fbc644592c614b3b5c1153b37f24
anupammaiti10 <maitianupam567@gmail.com>
2025-07-14 02:57:27 +0530

    Update app.js

--- Backend/app.js
+++ Backend/app.js
@@ -30,6 +30,7 @@ app.use(cors({
 }));
 
 app.use(express.static(path.join(__dirname, 'public')));
+app.set('views', path.join(__dirname, 'views'));
 
 // Add body parser middleware
 app.use(express.json());
