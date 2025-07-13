8fc451bb695428672c64c4477b2d18b772712c84
anupammaiti10 <maitianupam567@gmail.com>
2025-07-13 17:41:54 +0530

    Update script.js

--- Frontend/script.js
+++ Frontend/script.js
@@ -20,7 +20,7 @@ document.addEventListener('DOMContentLoaded', () => {
 async function initiateLogin() {
     showLoading(true);
     try {
-        console.log( window.ENV.FRONTEND_URL); 
+        console.log(window.ENV.FRONTEND_URL); 
         window.location.href = `${window.ENV.FRONTEND_URL}/users/auth`;
         
     } catch (error) {
