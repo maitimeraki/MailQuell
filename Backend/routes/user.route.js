a4301a0b68758f8add6318d53409e7c64d3b4ea4
anupammaiti10 <maitianupam567@gmail.com>
2025-08-26 00:34:16 +0530

    create a logout route

--- Backend/routes/user.route.js
+++ Backend/routes/user.route.js
@@ -1,11 +1,15 @@
 const express = require('express');
 const router = express.Router();
 
-router.get('/', (req, res) => {
-  res.render("index")
+router.post('/log-out', (req, res) => {
+  req.session.destroy(err => {
+    if (err) {
+      return res.status(500).json({ message: 'Logout failed' });
+    }
+    res.json({ message: 'Logout successful' });
+  });
+  res.clearCookie('connect.sid');
+  res.cookie("auth_token", "");
 });
 
-router.get('/home', (req, res) => {
-  res.render('home');
-});
 module.exports = router;
