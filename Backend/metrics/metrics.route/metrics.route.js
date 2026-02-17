e2424795c7883d352b760b5d4c481048967e1031
Anupam Maiti <bwubtd22190@brainwareuniversity.ac.in>
2026-02-17 18:45:35 +0530

    added custom prometheus middleware

--- Backend/metrics/metrics.route/metrics.route.js
+++ Backend/metrics/metrics.route/metrics.route.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const promClient = require("prom-client");
+const ResponseTime = require('response-time');
 
 
 // Prometheus metrics setup
@@ -8,6 +9,30 @@ const collectDefaultMetrics = promClient.collectDefaultMetrics;
 const Registry = promClient.Registry;
 const register = new Registry();
 collectDefaultMetrics({ register });
+
+// 1. Define a Histogram to track request duration
+const httpRequestDurationMicroseconds = new promClient.Histogram({
+    name: "http_request_duration_ms",
+    help: "Duration of HTTP requests in milliseconds",
+    labelNames: ["method", "route", "status_code"],
+    buckets: [50, 100, 200, 300, 400, 500, 750, 1000, 2000, 5000] // Buckets for response time in ms
+});
+
+register.registerMetric(httpRequestDurationMicroseconds);
+// 2. The Custom Monitor Middleware
+const ResponseMiddleware = () => {
+    return (req, res, next) => {
+        const recorder = ResponseTime((req, res, time) => {
+            const routePath = req.route ? req.route.path : req.originalUrl;
+            httpRequestDurationMicroseconds
+                .labels(req.method, routePath, res.statusCode)
+                .observe(time);
+        });
+        //Because next is passed into it, response-time immediately calls next(). So we can just return the middleware function it provides.
+        recorder(req,res, next);
+    };
+};
+
 
 router.get('/metrics', async (req, res) => {
     try {
@@ -20,4 +45,4 @@ router.get('/metrics', async (req, res) => {
     }
 });
 
-module.exports = router;
+module.exports = { metricsRouter: router, Response: ResponseMiddleware };
