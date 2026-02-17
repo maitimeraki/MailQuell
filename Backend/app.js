8d025110694def7065b4aa155d880661ad414174
Anupam Maiti <bwubtd22190@brainwareuniversity.ac.in>
2026-02-17 17:43:53 +0530

    Build a docker images using multi-stage process

--- Backend/app.js
+++ Backend/app.js
@@ -19,29 +19,28 @@ const webHookRoute = require('./routes/webHook.route');
 const userRoute = require("./routes/user.route");
 // const {autoLogin} = require("./middlewares/autoLogin");
 const tagsRoute = require('./routes/tagRoute.route');
-const { redisClient } = require("./config/redis")
+const { redisClient } = require("./config/redis");
+const { metricsRouter, Response } = require("./metrics/metrics.route/metrics.route");
 const profileRoute = require("./routes/profile.route");
 const { gmailWorkerHandler } = require("./workers/gmailWorker");
 const authenticationRoute = require("./routes/authentication.route");
-const metricsRoute = require("./metrics/metrics.route/metrics.route");
 //Database connection 
 async function initApp() {
-    try {
-        // 1. Connect to Database FIRST
-        await connectdb(); 
-        console.log("✅ Database connected successfully");
+  try {
+    // 1. Connect to Database FIRST
+    await connectdb();
+    console.log("✅ Database connected successfully");
 
-        // 2. Start Worker ONLY after DB is ready
-        gmailWorkerHandler(redisClient);
+    // 2. Start Worker ONLY after DB is ready
+    gmailWorkerHandler(redisClient);
 
-    } catch (err) {
-        console.error("❌ Failed to start app:", err);
-        process.exit(1);
-    }
+  } catch (err) {
+    console.error("❌ Failed to start app:", err);
+    process.exit(1);
+  }
 }
 
 initApp();
-
 const app = express();
 dotenv.config();
 
@@ -72,10 +71,10 @@ app.set('views', path.join(__dirname, 'views'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(helmet());                        // Set secure HTTP headers
+app.use(Response());
 app.use(xssClean());                      // Prevent XSS attacks
 app.use(mongoSanitize());                // Prevent NoSQL injection
 app.use(morgan('combined'));
-
 
 
 //Connect to localhost on port 6379.
@@ -90,7 +89,6 @@ app.use(morgan('combined'));
 
 
 // Add session middleware
-
 
 app.use(session({
   //If you have multiple apps using the same Redis instance, prefixes prevent conflicts
@@ -107,7 +105,7 @@ app.use(session({
   }
 }));
 
-app.use("/monitor", metricsRoute);
+app.use("/monitor", metricsRouter);
 app.use('/webhook', webHookRoute);
 app.use(profileRoute);
 app.use('/users', authenticationRoute);
