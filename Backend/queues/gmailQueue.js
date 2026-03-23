86b7f77471b53f00c99d8f417e14f40633173ac6
maitimeraki <anupammaiti10@gmail.com>
2026-03-23 23:18:55 +0530

    No changes

--- Backend/queues/gmailQueue.js
+++ Backend/queues/gmailQueue.js
@@ -1,9 +1,9 @@
-const {Queue} = require('bullmq');
+const { Queue } = require('bullmq');
 const { redisClient } = require('../config/redis');
 
 // Create a Queue
-const gmailQueue = new Queue("gmail-notifications",{
+const gmailQueue = new Queue("gmail-notifications", {
     connection: redisClient
 });
 
-module.exports = {gmailQueue};
+module.exports = { gmailQueue };
